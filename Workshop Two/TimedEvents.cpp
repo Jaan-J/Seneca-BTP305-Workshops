2a6d88f3dea7499b828afcd9a925836818369a3b
Jaan <jaan.javed447@gmail.com>
2021-09-23 15:14:41 -0400

    Completed most of the TimedEvents module

---
+++ Workshop Two/TimedEvents.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <iomanip>
+#include "TimedEvents.h"
+
+namespace sdds {
+	TimedEvents::TimedEvents() :t_numRecords(0u) {};
+
+	void TimedEvents::stopClock() {
+
+	}
+	void TimedEvents::startClock() {
+
+	}
+
+	void TimedEvents::addEvent(const char* ae) {
+		if (ae != nullptr && ae[0] != '\0' && t_numRecords < maxRecords) {
+			records[t_numRecords].s_eventName = ae;
+			records[t_numRecords].s_units = "nanoseconds";
+			records[t_numRecords].eventDuration = std::chrono::duration_cast <std::chrono::nanoseconds > (endtime - startTime);
+			t_numRecords++;
+		}
+	}
+
+	std::ostream& operator<<(std::ostream& os, TimedEvents& timedEvents) {
+		os << "Execution Times:" << std::endl;
+		os << "--------------------------" << std::endl;
+		for (size_t i = 0; i < timedEvents.t_numRecords; ++i) {
+			os << std::setw(20) << std::left << timedEvents.records[i].s_eventName <<
+								   std::setw(12) << std::right << timedEvents.records[i].eventDuration.count() << " " << 
+								   std::right << timedEvents.records[i].s_units << std::endl;
+		}
+		os << "--------------------------" << std::endl;
+		return os;
+	}
+}
--- Workshop Two/TimedEvents.h
+++ Workshop Two/TimedEvents.h
@@ -1,23 +1,24 @@
-#ifndef SDDS_TIME_EVENTS_H
-#define SDDS_TIME_EVETS_H
+#ifndef SDDS_TIMED_EVENTS_H
+#define SDDS_TIMED_EVETS_H
 #include<chrono>
 #include <string>
 namespace sdds {
-	const int maxRecords = 7;
+	const int maxRecords = 10;
 	class TimedEvents {
-		int t_numRecords;
+		size_t t_numRecords;
 		std::chrono::steady_clock::time_point startTime;
 		std::chrono::steady_clock::time_point endtime;
-		struct eventDetails {
+		struct{
 			std::string s_eventName;
-			std::string s_units = "seconds";
+			std::string s_units = "nanoseconds";
 			std::chrono::steady_clock::duration eventDuration;
-		};
+		}records[maxRecords];
 	public:
 		TimedEvents();
 		void startClock();
 		void stopClock();
-		friend std::ostream& operator<<(std::ostream&, TimedEvents);
+		void addEvent(const char*);
+		friend std::ostream& operator<<(std::ostream&, TimedEvents&);
 	};
 
 }
