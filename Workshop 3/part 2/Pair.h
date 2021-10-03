e0216db2f785f87a0eb2e2d191022973a45f54a2
Jaan-J <jaanmbjaved@gmail.com>
2021-10-03 15:46:12 -0400

    Added signature

--- Workshop 3/part 2/Pair.h
+++ Workshop 3/part 2/Pair.h
@@ -1,3 +1,8 @@
+/*
+Name: Jaan Javed
+Student Number: 100131192
+I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
+*/
 #ifndef SDDS_PAIR_H
 #define SDDS_PAIR_H
 #include<ostream>
--- Workshop 3/part 2/PairSummable.h
+++ Workshop 3/part 2/PairSummable.h
@@ -1,3 +1,8 @@
+/*
+Name: Jaan Javed
+Student Number: 100131192
+I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
+*/
 #ifndef SDDS_PAIRSUMMABLE_H
 #define SDDS_PAIRSUMMABLE_H
 #include "Pair.h"
